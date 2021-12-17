25aad1913d1f3293588c9235dae6a9b4e5c631c8
Greg Gomes <ggomes@gmail.com>
2021-12-17 11:48:51 -0800

    created sidebar and updated styling

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,12 +4,14 @@ import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
+import SideBar from './components/SideBar/SideBar.js';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import HomePage from './components/HomePage/HomePage.js';
 import OnePlaylist from './components/OnePlaylist/';
 import { authenticate } from './store/session';
+import { getSuserPlaylists } from './store/playlists';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -18,9 +20,11 @@ function App() {
   useEffect(() => {
     (async() => {
       await dispatch(authenticate());
+      await dispatch(getSuserPlaylists());
       setLoaded(true);
     })();
   }, [dispatch]);
+
 
   if (!loaded) {
     return null;
@@ -28,27 +32,28 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <HomePage loaded={loaded}/>
-        </ProtectedRoute>
-        <Route path='/playlists/:playlistId' exact={true}>
-          <OnePlaylist />
-        </Route>
-      </Switch>
+        <NavBar />
+        <SideBar />
+        <Switch>
+          <Route path='/login' exact={true}>
+            <LoginForm />
+          </Route>
+          <Route path='/sign-up' exact={true}>
+            <SignUpForm />
+          </Route>
+          <ProtectedRoute path='/users' exact={true} >
+            <UsersList/>
+          </ProtectedRoute>
+          <ProtectedRoute path='/users/:userId' exact={true} >
+            <User />
+          </ProtectedRoute>
+          <ProtectedRoute path='/' exact={true} >
+            <HomePage loaded={loaded}/>
+          </ProtectedRoute>
+          <Route path='/playlists/:playlistId' exact={true}>
+            <OnePlaylist />
+          </Route>
+        </Switch>
     </BrowserRouter>
   );
 }
--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -4,24 +4,25 @@ import { useHistory } from 'react-router-dom';
 import { getSuserPlaylists } from '../../store/playlists';
 
 const HomePage = () => {
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     const history = useHistory();
-    const [playlistsLoaded, setPlaylistsLoaded] = useState(false);
+    // const [playlistsLoaded, setPlaylistsLoaded] = useState(false);
     const user = useSelector(state => state.session.user);
     const playlists = useSelector(state => state.userPlaylists);
 
-    useEffect(() => {
-        (async () => {
-            await dispatch(getSuserPlaylists());
-            setPlaylistsLoaded(true);
-        })();
-    }, [dispatch]);
+    // useEffect(() => {
+    //     (async () => {
+    //         await dispatch(getSuserPlaylists());
+    //         setPlaylistsLoaded(true);
+    //     })();
+    // }, [dispatch]);
 
     const handlePlaylistClick = (playlistId) => {
         history.push(`/playlists/${playlistId}`)
     }
 
-    if (user && playlistsLoaded) {
+    // if (user && playlistsLoaded) {
+    if (user && playlists) {
         return (
             <>
                 <h1>Hello, {user.username}!</h1>
