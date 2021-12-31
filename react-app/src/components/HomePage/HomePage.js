971d811f3e6fd9c7bb1c762892e94a4880230b97
Greg Gomes <ggomes@gmail.com>
2021-12-31 15:02:32 -0800

    homepage frontend and backend get route for featured playlists

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector } from 'react-redux';
 // import React, { useEffect, useState } from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 // import { getSuserPlaylists } from '../../store/playlists';
 import './HomePage.css';
 
@@ -12,6 +12,9 @@ const HomePage = () => {
     // const [playlistsLoaded, setPlaylistsLoaded] = useState(false);
     const user = useSelector(state => state.session.user);
     const playlists = useSelector(state => state.userPlaylists);
+    const [featuredPlaylistsLoaded, setFeaturedPlaylistsLoaded] = useState(false);
+    const [featuredPlaylists, setFeaturedPlaylists] = useState("");
+    // const dispatch = useDispatch();
 
     // useEffect(() => {
     //     (async () => {
@@ -19,6 +22,17 @@ const HomePage = () => {
     //         setPlaylistsLoaded(true);
     //     })();
     // }, [dispatch]);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("/api/playlists/featured")
+
+            if (response.ok) {
+                const featuredlists = await response.json();
+                setFeaturedPlaylists(Object.values(featuredlists))
+                setFeaturedPlaylistsLoaded(true);
+            }
+        })();
+    }, []);
 
     const handlePlaylistClick = (playlistId) => {
         history.push(`/playlists/${playlistId}`)
@@ -29,13 +43,56 @@ const HomePage = () => {
         return (
             <div className="homepage-container">
                 <h1>Hello, {user.username}!</h1>
-                {playlists.map(playlist => {
-                    return (
-                        <div className="playlist-link" key={playlist.id} onClick={() => handlePlaylistClick(playlist.id)}>{playlist.name}</div>
-                    )
-                })}
+                {playlists.length === 0 && "<== Click 'Create Playlist' to start a new playlist"}
+                <div className="playlist-row">
+                    {playlists.map((playlist, i) => {
+                        if (i < 4) {
+                            return (
+                                <div className="paylist-container" key={playlist.id}>
+                                    <div className="cover-container" onClick={() => handlePlaylistClick(playlist.id)}><img className="playlist-cover" src={playlist.pic}></img></div>
+                                    <div className="playlist-link" onClick={() => handlePlaylistClick(playlist.id)}>{playlist.name}</div>
+                                </div>
+                            )
+                        }
+                    })}
+                </div>
+                {featuredPlaylistsLoaded && featuredPlaylists && (
+                    <div>
+                        <h1>Featured Playlists</h1>
+                        <div className="playlist-row">
+                            {featuredPlaylists.map(playlist => {
+                                return (
+                                    <div className="playlist-container" key={playlist.id}>
+                                        <div className="cover-container" onClick={() => handlePlaylistClick(playlist.id)}><img className="playlist-cover" src={playlist.pic}></img></div>
+                                        <div className="playlist-link" onClick={() => handlePlaylistClick(playlist.id)}>{playlist.name}</div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )}
             </div>
         )
+    } else if (!user && featuredPlaylistsLoaded) {
+        return (
+            <div className="homepage-container">
+                {featuredPlaylists && (
+                    <div>
+                        <h1>Featured Playlists</h1>
+                        <div className="playlist-row">
+                            {featuredPlaylists.map(playlist => {
+                                return (
+                                    <div className="playlist-container">
+                                        <div className="cover-container" onClick={() => handlePlaylistClick(playlist.id)}><img className="playlist-cover" src={playlist.pic}></img></div>
+                                        <div className="playlist-link" onClick={() => handlePlaylistClick(playlist.id)}>{playlist.name}</div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
     } else {
         return null;
     }
